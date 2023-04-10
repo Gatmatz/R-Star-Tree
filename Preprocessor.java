19b9186c3c0ce9da93d8881a449136d1637abf12
Georgios Atmatzidis <gatmatzidi@gmail.com>
2023-04-10 04:19:14 +0300

    Added file conversion and data folder with test-files.

---
+++ Preprocessor.java
@@ -0,0 +1,80 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+/**
+ * Java Class that converts an XML(OSM) file to CSV for a better processing afterwards.
+ */
+public class Preprocessor {
+    /**
+     * Boolean method that converts an XML(OSM) file to CSV.
+     * @param filePath  The path of input file.
+     * @param outputPath    The path of the output file.
+     * @return true it conversion was successful, false otherwise.
+     */
+    public boolean convertToCSV(String filePath,String outputPath)
+    {
+        try {
+            //Parse XML file for conversion
+            File OSMFile = new File(filePath);
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder;
+            docBuilder = docFactory.newDocumentBuilder();
+            Document osmDoc = docBuilder.parse(OSMFile);
+            osmDoc.getDocumentElement().normalize();
+
+            //Initialize xPath
+            XPath xPath =  XPathFactory.newInstance().newXPath();
+
+            //Set xPath in XML file
+            String path = "/osm/node";
+            //Get node list in the specific path
+            NodeList nodeList = (NodeList) xPath.compile(path).evaluate(
+                    osmDoc, XPathConstants.NODESET);
+
+            //Write metadata to .csv document
+            FileWriter myWriter = new FileWriter(outputPath);
+            myWriter.write("id,lat,lon\n");
+
+            //Iterate through nodes and write the needed attributes of node in the csv file
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node nNode = nodeList.item(i);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE)
+                {
+                    Element eElement = (Element) nNode;
+                    String line = eElement.getAttribute("id")+','+eElement.getAttribute("lat")+','+eElement.getAttribute("lon");
+                    myWriter.write(line+'\n');
+                }
+            }
+
+            //Writing ended
+            myWriter.close();
+
+            //Inform that everything went well
+            return true;
+
+        } catch (ParserConfigurationException | SAXException | IOException | XPathExpressionException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public static void main(String[] args)
+    {
+        String filePath = "data/small.xml";
+        String outputPath = "data/small_processed.csv";
+        Preprocessor pre = new Preprocessor();
+        if (pre.convertToCSV(filePath,outputPath))
+            System.out.println("Successful conversion!");
+        else
+            System.out.println("An error occurred!");
+    }
+}
