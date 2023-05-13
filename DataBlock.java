2d02d725bef85edcc2cdf1b22de5f570e6abbeae
nefelipar <marianep@csd.auth.gr>
2023-05-13 16:50:52 +0300

    comments

--- DataBlock.java
+++ DataBlock.java
@@ -1,20 +1,37 @@
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * Java Class that represents a data block which consists of records.
+ */
 public class DataBlock implements Serializable
 {
-    public int blockId;
+    public long blockId;
     public ArrayList<Record> records;
-    public DataBlock(int blockId)
+
+    /**
+     *A constructor for a data block  that takes as parameters the id of the block.
+     * @param blockId block id.
+     */
+    public DataBlock(long blockId)
     {
         this.blockId=blockId;
         records=new ArrayList<>();
     }
+
+    /**
+     * Adds a new record to a specific block.
+     * @param n the record I want to add to this block.
+     */
     public void addRecord(Record n)
     {
         records.add(n);
     }
 
+    /**
+     * Gets the size of an ArrayList of Records.
+     * @return long size from the Arraylist of Records.
+     */
     public long getNofRecords()
     {
         return records.size();
--- Record.java
+++ Record.java
@@ -1,18 +1,27 @@
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
 
+/**
+ * Java Class that represents a record.
+ */
 public class Record implements Serializable{
     public long id;
     public ArrayList<Double> coordinates;
 
-    public Record(long id, ArrayList<Double> cor){
+    /**
+     *A constructor for a record that takes as parameters the id of the record and its coordinates.
+     * @param id record id.
+     * @param cor the coordinates.
+     */
+    public Record(long id, ArrayList<Double> cor)
+    {
         this.id=id;
         coordinates=new ArrayList<>();
         coordinates.addAll(cor);
     }
 
-    public void printIt(){
+    public void printIt()
+    {
         System.out.println(id);
         for (Double c: coordinates){
             System.out.println(c);
